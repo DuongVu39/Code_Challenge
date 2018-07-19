55ae33a85ae6ca83f44ad5051e5817a3706baedf
duongvu39 <dqvu@umail.iu.edu>
2018-07-18 21:21:02 -0700

    update code comments to explain the choice and fix minor bug

--- src/task1/create_labels.py
+++ src/task1/create_labels.py
@@ -26,13 +26,22 @@ def create_labels(df, time_range=40):  # Tested [Y]
     # Reset index
     wind_turbine = wind_turbine.reset_index().iloc[:,1:29]
     
+    # Initial value for accumulate status column: mark
+    wind_turbine['mark'] = 0 
+    
     # Creating label:
     for i in range(1,time_range+1):
+        # Create a status_shift column contain the status of the next i day:
         wind_turbine["status_shift"] = wind_turbine.groupby("unit_number")['status'].transform(lambda g: g.shift(periods=-i))
-        wind_turbine['mark'] = 0 # default value
+        
+        # Add the status of day i to the accumulate status column:
         wind_turbine['mark'] = wind_turbine['mark'] + np.where(wind_turbine["status_shift"]==0.0, 0, 1)
-    
+        
+    # Check if there is any fail during the time range, if not, the accumulate status column should be 0.9
     wind_turbine['label'] = np.where(wind_turbine["mark"]==0.0, 0, 1)
+    
+    # Exclude all unnecessary columns:
     final_col = [ i for i in wind_turbine.columns if i not in ['mark', 'status_shift']]
+    
     return wind_turbine[final_col]
     
