c0243300bcf7947e92f2d0d2e7feef6c8cb161f9
duongvu39 <dqvu@umail.iu.edu>
2018-07-20 16:54:09 -0700

    update stuff

--- src/task1/get_dummies.py
+++ src/task1/get_dummies.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import numpy as np
 
-def get_dummies(df):  # Tested [N]
+def get_dummies(df, col_name):  # Tested [N]
 
     """
     Transform categorical variable to dummies and select one less level to prevent colinearity 
 
     Args:
         df (dataframe): Original dataframe 
+        col_name(str): Name of column to be transformed
         
     Returns:
         df (dataframe): New dataframe with no categorical feature.
     """
     
-    df_new = pd.get_dummies(df).iloc[:,:-1]
+    df[col_name] = pd.get_dummies(df[col_name]).iloc[:,0]
     
-    return df_new
+    return df
